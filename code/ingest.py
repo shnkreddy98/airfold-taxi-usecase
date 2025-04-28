7eeea793896c786cd2f691824bf71f8bb89a3009
shashank <sreddy2095@gmail.com>
2025-04-28 14:37:35 -0700

    final changes done, ingest data will work

---
+++ code/ingest.py
@@ -0,0 +1,136 @@
+from dask.distributed import Client
+from dotenv import load_dotenv
+from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
+
+import logging
+import json
+import os
+import pandas as pd
+import requests
+
+load_dotenv()
+
+data_dir = './trip-data'
+transformed_data_dir = os.path.join(data_dir, "new_data")
+if not os.path.exists(transformed_data_dir):
+    os.makedirs(transformed_data_dir)
+
+def append_source(table, filename):
+    logging.info("Appending {} data to airfold to table {}".format(filename,table))
+    auth = os.getenv('auth_code')
+
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    url = 'https://api.us.airfold.co/v1/events/{}'
+
+    res = requests.post(
+        url.format(table),
+        headers={
+            'Authorization': 'Bearer {}'.format(auth),
+            'Content-Type': 'application/json'
+        },
+        json=data
+    )
+    if not str(res.status_code).starts_with('2'):
+        logging.error(f"Data not appended for file {filename}")
+
+def true_false_values(tablename, df):
+    if tablename == "dim_time":
+        return df.map(lambda x: 1 if x else 0) if hasattr(df, "applymap") else df.apply(lambda x: 1 if x else 0)
+    else:
+        return df.map(lambda x: 1 if x == 'Y' else 0) if hasattr(df, "applymap") else df.apply(lambda x: 1 if x == 'Y' else 0)
+
+def transform_data(data):
+    # dim_time
+    dim_time = data[['request_datetime', 'on_scene_datetime', 'pickup_datetime', 'dropoff_datetime']].melt(
+        value_name='full_datetime'
+    )[['full_datetime']]
+    dim_time = dim_time.drop_duplicates().reset_index(drop=True)
+
+    dim_time['date'] = dim_time.full_datetime.dt.date
+    # dim_time['time'] = dim_time.full_datetime.dt.time
+    dim_time['hour'] = dim_time.full_datetime.dt.hour
+    dim_time['minute'] = dim_time.full_datetime.dt.minute
+    dim_time['second'] = dim_time.full_datetime.dt.second
+    dim_time['year'] = dim_time.full_datetime.dt.year
+    dim_time['quarter'] = dim_time.full_datetime.dt.quarter
+    dim_time['month'] = dim_time.full_datetime.dt.month
+    dim_time['day_of_month'] = dim_time.full_datetime.dt.day
+    dim_time['day_of_week'] = dim_time.full_datetime.dt.day_of_week
+    dim_time['day_name'] = dim_time.full_datetime.dt.day_name()
+    dim_time['is_weekend'] = dim_time.day_of_week > 4
+    cal = calendar()
+    holidays = cal.holidays(start = dim_time['date'].min(),
+                            end = dim_time['date'].max())
+
+    dim_time['is_holiday'] = dim_time['date'].isin(holidays)
+
+    dim_time[['full_datetime', 'date']] = dim_time[['full_datetime', 'date']].astype('str')
+    dim_time[["is_weekend", "is_holiday"]] = true_false_values("dim_time", dim_time[["is_weekend", "is_holiday"]])
+
+    # fact_trips
+    data.rename(columns={
+        'dispatching_base_num': 'dispatching_base_id', 
+        'originating_base_num': 'originating_base_id',
+        'request_datetime': 'request_time_id', 
+        'on_scene_datetime': 'on_scene_time_id', 
+        'pickup_datetime': 'pickup_time_id',
+        'dropoff_datetime': 'dropoff_time_id', 
+        'PULocationID': 'pickup_location_id', 
+        'DOLocationID': 'dropoff_location_id'
+        }, inplace=True)
+
+    fact_trips = data[['trip_id', 'hvfhs_license_num', 'dispatching_base_id', 
+                    'originating_base_id', 'request_time_id', 'on_scene_time_id', 
+                    'pickup_time_id', 'dropoff_time_id', 'pickup_location_id', 
+                    'dropoff_location_id', 'trip_miles', 'trip_time', 
+                    'base_passenger_fare', 'tolls', 'bcf', 'sales_tax',
+                    'congestion_surcharge', 'airport_fee', 'tips', 'driver_pay',
+                    'shared_request_flag', 'shared_match_flag', 'access_a_ride_flag',
+                    'wav_request_flag', 'wav_match_flag']].drop_duplicates().reset_index(drop=True)
+
+    fact_trips[['shared_request_flag', 'shared_match_flag', 'access_a_ride_flag',
+                'wav_request_flag', 'wav_match_flag']] = true_false_values("fact_trips", 
+                                                                           fact_trips[['shared_request_flag', 'shared_match_flag', 
+                                                                                       'access_a_ride_flag', 'wav_request_flag', 
+                                                                                       'wav_match_flag']])
+
+    # Write to csv
+    filename = "{}{}{}"
+    max_time = str(dim_time.full_datetime.max())
+    max_time = max_time.replace("-", "").replace(" ", "_").replace(":", "")
+
+    dim_time_filename = os.path.join(transformed_data_dir, filename.format("dim_time_", max_time, ".json"))
+    fact_trips_filename = os.path.join(transformed_data_dir, filename.format("fact_trips_", max_time, ".json"))
+
+    dim_time.to_json(dim_time_filename, orient='records')
+    fact_trips.to_json(fact_trips_filename, orient='records')
+    # dim_time.to_csv(dim_time_filename, index=False)
+    # fact_trips.to_csv(fact_trips_filename, index=False)
+
+    # Call af source append
+    append_source("dim_time", dim_time_filename)
+    append_source("fact_trips", fact_trips_filename)
+
+
+def ingest_data(file):
+    data = pd.read_parquet(os.path.join(data_dir, file))
+    data = data.dropna().reset_index(drop=True)
+    data = data.drop_duplicates().reset_index(drop=True)
+    data['trip_id'] = data.index
+
+    client = Client()
+    step = 10000
+    end = data.shape[0]
+
+    logging.info("Starting parallel transformation")
+    for start in range(0, end, step):
+        if start+step>end:
+            client.submit(transform_data, data[start:end].copy())
+            # transform_data(data[start:end].copy())
+        else:
+            client.submit(transform_data, data[start:start+step].copy())
+            # transform_data(data[start:start+step].copy())
+    
+if __name__=="__main__":
+    ingest_data()
--- code/main.py
+++ code/main.py
@@ -1,138 +1,44 @@
-from dask.distributed import Client
-from dotenv import load_dotenv
-import json
+from ingest import ingest_data
+from logging_utils import setup_logging
+
+import logging
 import os
-import pandas as pd
-from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
-import requests
-import sys
 
-load_dotenv()
+def read_idx():
+    with open("file_idx.txt", "r") as f:
+        idx = int(f.read())
+    return idx
 
-data_dir = './trip-data'
-transformed_data_dir = os.path.join(data_dir, "new_data")
-if not os.path.exists(transformed_data_dir):
-    os.makedirs(transformed_data_dir)
+def write_idx(idx):
+    with open("file_idx.txt", "w+") as f:
+        f.write(idx)
 
-def append_source(table, filename):
-    print("Appending {} data to airfold to table {}".format(filename,table))
-    auth = os.getenv('auth_code')
+if __name__ == "__main__":
+    data_files = ["fhvhv_tripdata_2023-01.parquet", "fhvhv_tripdata_2023-02.parquet", 
+                "fhvhv_tripdata_2023-03.parquet", "fhvhv_tripdata_2023-04.parquet", 
+                "fhvhv_tripdata_2023-05.parquet", "fhvhv_tripdata_2023-06.parquet", 
+                "fhvhv_tripdata_2023-07.parquet", "fhvhv_tripdata_2023-08.parquet", 
+                "fhvhv_tripdata_2023-09.parquet", "fhvhv_tripdata_2023-10.parquet", 
+                "fhvhv_tripdata_2023-11.parquet", "fhvhv_tripdata_2023-12.parquet", 
+                "fhvhv_tripdata_2024-01.parquet", "fhvhv_tripdata_2024-02.parquet", 
+                "fhvhv_tripdata_2024-03.parquet", "fhvhv_tripdata_2024-04.parquet", 
+                "fhvhv_tripdata_2024-05.parquet", "fhvhv_tripdata_2024-06.parquet", 
+                "fhvhv_tripdata_2024-07.parquet", "fhvhv_tripdata_2024-08.parquet", 
+                "fhvhv_tripdata_2024-09.parquet", "fhvhv_tripdata_2024-10.parquet", 
+                "fhvhv_tripdata_2024-11.parquet", "fhvhv_tripdata_2024-12.parquet"]
 
-    with open(filename, 'r') as f:
-        data = json.load(f)
-    url = 'https://api.us.airfold.co/v1/events/{}'
+    log_file = setup_logging()
 
-    res = requests.post(
-        url.format(table),
-        headers={
-            'Authorization': 'Bearer {}'.format(auth),
-            'Content-Type': 'application/json'
-        },
-        json=data
-    )
-    if 200<=res.status_code<300:
-        print("Data Appended")
-    else:
-        print("Error %s", res.status_code)
+    try:
+        idx = read_idx()
+    except:
+        idx = 0
+        write_idx(idx)
 
-def true_false_values(tablename, df):
-    if tablename == "dim_time":
-        return df.map(lambda x: 1 if x else 0) if hasattr(df, "applymap") else df.apply(lambda x: 1 if x else 0)
-    else:
-        return df.map(lambda x: 1 if x == 'Y' else 0) if hasattr(df, "applymap") else df.apply(lambda x: 1 if x == 'Y' else 0)
+    file = data_files[idx]
 
-def transform_data(data):
-    # dim_time
-    dim_time = data[['request_datetime', 'on_scene_datetime', 'pickup_datetime', 'dropoff_datetime']].melt(
-        value_name='full_datetime'
-    )[['full_datetime']]
-    dim_time = dim_time.drop_duplicates().reset_index(drop=True)
+    logging.info(f"Ingesting {file} to airfold")
+    ingest_data(file)
 
-    dim_time['date'] = dim_time.full_datetime.dt.date
-    # dim_time['time'] = dim_time.full_datetime.dt.time
-    dim_time['hour'] = dim_time.full_datetime.dt.hour
-    dim_time['minute'] = dim_time.full_datetime.dt.minute
-    dim_time['second'] = dim_time.full_datetime.dt.second
-    dim_time['year'] = dim_time.full_datetime.dt.year
-    dim_time['quarter'] = dim_time.full_datetime.dt.quarter
-    dim_time['month'] = dim_time.full_datetime.dt.month
-    dim_time['day_of_month'] = dim_time.full_datetime.dt.day
-    dim_time['day_of_week'] = dim_time.full_datetime.dt.day_of_week
-    dim_time['day_name'] = dim_time.full_datetime.dt.day_name()
-    dim_time['is_weekend'] = dim_time.day_of_week > 4
-    cal = calendar()
-    holidays = cal.holidays(start = dim_time['date'].min(),
-                            end = dim_time['date'].max())
-
-    dim_time['is_holiday'] = dim_time['date'].isin(holidays)
-
-    dim_time[['full_datetime', 'date']] = dim_time[['full_datetime', 'date']].astype('str')
-    dim_time[["is_weekend", "is_holiday"]] = true_false_values("dim_time", dim_time[["is_weekend", "is_holiday"]])
-
-    # fact_trips
-    data.rename(columns={
-        'dispatching_base_num': 'dispatching_base_id', 
-        'originating_base_num': 'originating_base_id',
-        'request_datetime': 'request_time_id', 
-        'on_scene_datetime': 'on_scene_time_id', 
-        'pickup_datetime': 'pickup_time_id',
-        'dropoff_datetime': 'dropoff_time_id', 
-        'PULocationID': 'pickup_location_id', 
-        'DOLocationID': 'dropoff_location_id'
-        }, inplace=True)
-
-    fact_trips = data[['trip_id', 'hvfhs_license_num', 'dispatching_base_id', 
-                    'originating_base_id', 'request_time_id', 'on_scene_time_id', 
-                    'pickup_time_id', 'dropoff_time_id', 'pickup_location_id', 
-                    'dropoff_location_id', 'trip_miles', 'trip_time', 
-                    'base_passenger_fare', 'tolls', 'bcf', 'sales_tax',
-                    'congestion_surcharge', 'airport_fee', 'tips', 'driver_pay',
-                    'shared_request_flag', 'shared_match_flag', 'access_a_ride_flag',
-                    'wav_request_flag', 'wav_match_flag']].drop_duplicates().reset_index(drop=True)
-
-    fact_trips[['shared_request_flag', 'shared_match_flag', 'access_a_ride_flag',
-                'wav_request_flag', 'wav_match_flag']] = true_false_values("fact_trips", 
-                                                                           fact_trips[['shared_request_flag', 'shared_match_flag', 
-                                                                                       'access_a_ride_flag', 'wav_request_flag', 
-                                                                                       'wav_match_flag']])
-
-    # Write to csv
-    filename = "{}{}{}"
-    max_time = str(dim_time.full_datetime.max())
-    max_time = max_time.replace("-", "").replace(" ", "_").replace(":", "")
-
-    dim_time_filename = os.path.join(transformed_data_dir, filename.format("dim_time_", max_time, ".json"))
-    fact_trips_filename = os.path.join(transformed_data_dir, filename.format("fact_trips_", max_time, ".json"))
-
-    dim_time.to_json(dim_time_filename, orient='records')
-    fact_trips.to_json(fact_trips_filename, orient='records')
-    # dim_time.to_csv(dim_time_filename, index=False)
-    # fact_trips.to_csv(fact_trips_filename, index=False)
-
-    # Call af source append
-    append_source("dim_time", dim_time_filename)
-    append_source("fact_trips", fact_trips_filename)
-
-
-def main():
-    file = sys.argv[1]
-    data = pd.read_parquet(os.path.join(data_dir, file))
-    data = data.dropna().reset_index(drop=True)
-    data = data.drop_duplicates().reset_index(drop=True)
-    data['trip_id'] = data.index
-
-    client = Client()
-    step = 10000
-    end = data.shape[0]
-
-    print("Starting parallel transformation")
-    for start in range(0, end, step):
-        if start+step>end:
-            client.submit(transform_data, data[start:end].copy())
-            # transform_data(data[start:end].copy())
-        else:
-            client.submit(transform_data, data[start:start+step].copy())
-            # transform_data(data[start:start+step].copy())
-    
-if __name__=="__main__":
-    main()
+    logging.info("Data Ingested")
+    write_idx(idx+1)
